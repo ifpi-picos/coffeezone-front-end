4dd02145d721996d5efb527a21a612eaaf3faa12
GabrielGomesdS <capic.2021118tads0297@aluno.ifpi.edu.br>
2022-06-01 20:33:14 -0300

    v1.0.2 adição da tokenização no js e pequena alteracão no css no userprofilescreen

--- scripts/userprofilescreen.js
+++ scripts/userprofilescreen.js
@@ -1,24 +1,41 @@
-const infoname = document.querySelector('h2[type="text"]')
-const infofuction = document.querySelector('h2[type="text"]')
-const infomember = document.querySelector('h2[type="text"]')
-const infoemail = document.querySelector('h2[type="email"]')
-const infophone = document.querySelector('h2[type="phone"]')
-const infolinkedin = document.querySelector('h2[type="linkedin"}')
+/*var myHeaders = new Headers();
+myHeaders.append("Authorization", "TokenProvidedOnLogin");*/
 
-var raw = "{\r\n    \"name\": \"User Complete Name\",\r\n    \"email\": \"user@email.com\",\r\n    \"phone\": \"+5589999999999\",\r\n    \"password\": \"userpassword\",\r\n    \"occupation\": \"User occupation\",\r\n    \"type\": \"Member\",\r\n    \"linkedin\": \"https://www.linkedin.com/in/userlinkedin/\" //Optional\r\n}";
+const infoname = document.querySelector('.infoname')
+const infofuction = document.querySelector('.infofuction')
+const infomember = document.querySelector('.infomember')
+const infoemail = document.querySelector('.infoemail')
+const infophone = document.querySelector('.infophone')
+const infolinkedin = document.querySelector('.infolinkedin')
+
+const token = localStorage.getItem('token')
+
+/*var raw = "{\r\n    \"name\": \"User Complete Name\",\r\n    \"email\": \"user@email.com\",\r\n    \"phone\": \"+5589999999999\",\r\n    \"password\": \"userpassword\",\r\n    \"occupation\": \"User occupation\",\r\n    \"type\": \"Member\",\r\n    \"linkedin\": \"https://www.linkedin.com/in/userlinkedin/\" //Optional\r\n}";*/
 
 var requestOptions = {
-  method: 'get',
-  body: raw,
-  Headers: {
+  method: 'GET',
+  headers: {
     Accept: 'application/json',
       'Content-Type': 'application/json',
-    Authorization:
-      'U2FsdGVkX18Bp/qsNbHkhpvuUTb/+ahxa+KPSIKV4/nc9Qr92WTJFQbX5jbcCTYGv4JX0k47uqh+UF1PZp5ysGujq6Ob4g0LlZwCU3t4adYulN5e2PcmmEkL6aPrCkFi3u+ZDHiM0NKWUH2UkMhX/xlMFXfbE60HjYeKDdBOcNP4tRDFaTgo3TWKo4yI7+8LoE+9WxLXOsldKPEhMepLZyjPupOzGqWzHMH2cnwH2s/VrRSBw56UMZVpNTFNGseMva1i4ZBfZErlsFkaohy96MQqBT/NpsYXCIuKgzubrqU='
+    Authorization: token
     }
 };
 
-fetch("{{/coffeezone-backend.herokuapp.com/}}/user/", requestOptions)
+let infos;
+
+function useInfos(a){
+  infoname.innerText = a.name
+  infoemail.innerText = a.email
+  infofuction.innerText = a.occupation
+  infomember.innerText = a.type
+  infophone.innerText = a.phone
+  infolinkedin.innerText = a.linkedin
+}
+
+fetch("https://coffeezone-backend.herokuapp.com/user/", requestOptions)
   .then(response => response.text())
-  .then(result => console.log(JSON.parce(result)))
+  .then(result => {
+    infos = JSON.parse(result)
+    useInfos(infos)
+  })
   .catch(error => console.log('error', error));
