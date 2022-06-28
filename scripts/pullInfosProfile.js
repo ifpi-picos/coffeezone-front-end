cc430d7e6bbe571b27641b9f19453654c44ec3e7
Ítalo Paixão Gomes <italopaxg@gmail.com>
2022-06-28 10:40:38 -0300

    retirando comentários do código

--- scripts/pullInfosProfile.js
+++ scripts/pullInfosProfile.js
@@ -9,33 +9,8 @@ function useInfos (infosJson){
   if(infosJson.cardid) divDataPerson.insertAdjacentHTML('beforeend', `<p>Id do cartão: ${infosJson.cardid}</p>`)
 }
 
-/* const token = localStorage.getItem('token') */
-
-/* const requestOptions = {
-  method: 'GET',
-  headers: {
-    Accept: 'application/json',
-      'Content-Type': 'application/json',
-    Authorization: token
-    }
-}; */
-
 fetch("https://coffeezone-backend.herokuapp.com/user/", requestOptions)
   .then(response => response.json())
   .then(json => {
     useInfos(json)
   })
-
-  /* 
-  
-  cardid: null
-  email: "email1@email.com"
-  id: 2
-  linkedin: null
-  name: "Italo Paixão Gomes"
-  occupation: "front end"
-  preferences: {sendActionRegEmail: true}
-  profileimage: null
-  type: "Coordinator"
-  
-  */
